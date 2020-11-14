ca86c14897c177b308f4076b160e73114f0d019b
alfonso-miguel-uia <alfonso-miguel-uia@gmail.com>
2020-11-13 18:27:48 -0600

    Se construyo registraCheque y notaDebito

---
+++ src/main/java/uia/com/contabilidad/cuentas/Cuenta.java
@@ -0,0 +1,34 @@
+package uia.com.contabilidad.cuentas;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import uia.com.contabilidad.clientes.InfoUIA;
+
+public class Cuenta extends InfoUIA
+{
+		String fecha = "";
+		
+		@JsonCreator
+	    public Cuenta(@JsonProperty("id")int id, @JsonProperty("name")String name, @JsonProperty("fecha")String p1) 
+		{
+	        super(id, name);
+	        this.fecha = p1;
+	    }
+
+		public String getFecha() {
+			return fecha;
+		}
+
+		public void setFecha(String fecha) {
+			this.fecha = fecha;
+		}	
+		
+		
+
+		public void validaCobranza()
+		{
+		}
+}
