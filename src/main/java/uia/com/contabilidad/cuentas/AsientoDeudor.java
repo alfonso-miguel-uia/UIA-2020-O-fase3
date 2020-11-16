ad9b75145b2b3c30f8c069db73a1c3b847f174d2
alfonso-miguel-uia <alfonso-miguel-uia@gmail.com>
2020-11-16 12:50:11 -0600

    Probando la rama de alfonso solamente

---
+++ src/main/java/uia/com/contabilidad/cuentas/AsientoDeudor.java
@@ -0,0 +1,11 @@
+package uia.com.contabilidad.cuentas;
+
+import uia.com.contabilidad.clientes.InfoUIA;
+
+public class AsientoDeudor  extends InfoUIA{
+
+	public void AsientoDeudor()
+	{
+		
+	}
+}
